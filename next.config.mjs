a43dfeeaa9717529ee75451441279160039da165
ubaidahmed04 <obaidkhanzada2004@gmail.com>
2024-10-03 23:21:11 -0700

    change template

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'images.unsplash.com',
-            port: '',
-            pathname: '/**',
-          },
-        ],
-      },
-};
+const nextConfig = {};
 
 export default nextConfig;
