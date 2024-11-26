d07023fcf80cbbec50101d5e0f2142b3b24feafb
ubaidahmed04 <obaidkhanzada2004@gmail.com>
2024-11-26 12:21:28 +0500

    update  nextconfig file

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async redirects() {
+      return [
+        {
+          source: '/:path*', // Match all paths
+          has: [
+            {
+              type: 'host',
+              value: 'shopeco.vercel.app', // Match subdomain
+            },
+          ],
+          destination: 'https://ubaidahmed.store/:path*', // Redirect to the main domain
+          permanent: true, // Indicates a 308 Permanent Redirect
+        },
+      ];
+    },
     images: {
         domains: ['res.cloudinary.com'],
       },
