8e686eafd6866709a81a6648684dfb606699a9c4
ubaidahmed04 <obaidkhanzada2004@gmail.com>
2024-11-27 13:14:32 +0500

    update not found page

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -4,7 +4,6 @@ import Link from 'next/link';
 const NotFoundPage = () => {
   return (
    <div className='flex flex-col justify-center items-center w-full h-screen'>
-    <div>
         <h1 className="text-6xl font-bold text-red-500 text-center">404</h1>
         <h1 className='text-2xl fontbold text-secondary text-center'> Page Not Found</h1>
         <p className="mt-2 text-gray-600">The page you are looking for does not exist.</p>
@@ -13,7 +12,6 @@ const NotFoundPage = () => {
           Go back to Home
         </Link>
         </span>
-    </div>
    </div>
   );
 };
