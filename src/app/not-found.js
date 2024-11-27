c7617e41216cb239d8744b82218da453920d8b7f
ubaidahmed04 <obaidkhanzada2004@gmail.com>
2024-11-27 13:11:35 +0500

    update not found page

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -8,9 +8,11 @@ const NotFoundPage = () => {
         <h1 className="text-6xl font-bold text-red-500 text-center">404</h1>
         <h1 className='text-2xl fontbold text-secondary text-center'> Page Not Found</h1>
         <p className="mt-2 text-gray-600">The page you are looking for does not exist.</p>
+        <span className='mt-4 text-gray-600'> 
         <Link href="/" className="mt-4 px-6 py-2 text-xl myfont bg-secondary text-white  rounded-lg hover:bg-blue-600 transition duration-200">
-        <p className='mt-2 text-gray-600'> Go back to Home </p>
+          Go back to Home
         </Link>
+        </span>
     </div>
    </div>
   );
