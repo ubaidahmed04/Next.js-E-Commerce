364d9b275c4608d6e63ecfefe56ea58346dfbca9
ubaidahmed04 <obaidkhanzada2004@gmail.com>
2024-11-27 10:31:23 +0500

    add category wise filter product

--- src/app/(dashboard)/products/page.js
+++ src/app/(dashboard)/products/page.js
@@ -8,13 +8,14 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getProductStart, getProductSuccess } from '@/app/Redux/Slices/allProducts';
 import { addToCart } from '@/app/Redux/Slices/addToCart';
 import { DefaultPagination } from './Paginition';
+import { Option, Select ,Button} from '@material-tailwind/react';
+import Link from 'next/link';
 
 const Product = () => {
   const [limit, setLimit] = useState(10);
   const [skip, setSkip] = useState(0);
+  const [category, setCategory] = useState("");
   const dispatch = useDispatch()
-  // let limit = 10
-  // let skip = 0
   const nextPage =()=>{
     setSkip((prevSkip) => prevSkip + limit);
   }
@@ -23,46 +24,71 @@ const Product = () => {
   }
   const {allProducts,isLoader} = useSelector((state)=> state.allproducts)
   // console.log("redux allProducts",allProducts)
+
   const getAllProduct = async() => {
-    const route = `/product?limit=${limit}&skip=${skip}`;  
+    const route = `/product?limit=${limit}&skip=${skip}${category ? `&category=${category}` : ""}`;  
     try {
       dispatch(getProductStart())
         const response = await getAllProducts(route)
-        // console.log("response--->>>>", response)
+        console.log("response--->>>>", response)
         dispatch(getProductSuccess(response.data))
-        
-        
       } catch (error) {
         errorNotify(error ||response.message)
       }
   
   }
+  // add tocart 
   const handleAddToCart = (product) => {
     dispatch(addToCart(product));
   };
+  // filter product by category 
+  const handleCategoryChange = (value) =>{
+    setCategory(value)
+    setSkip(0)
+  }
   useEffect(()=>{
     getAllProduct()
-  },[limit,skip])
+  },[limit,skip,category ])
 
   return (
     <Suspense fallback={<ProductSkeleton/>}>
      {   isLoader ?<div className='  w-full  mx-auto '><ProductSkeleton/></div>:
-      <div className='font grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  justify-items-center gap-4 pb-10 pt-4 flex-grow max-w-screen-xl  w-full min-h-screen max-h-full '>
      
-       {
-        // isLoader ?<div className='  w-full  mx-auto '><Loader/></div>:
+        allProducts.length < 1 ? <div className="flex justify-center h-screen items-center max-w-4xl text-4xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">Currently, there are no products available. Stay tuned—exciting new items are coming soon! <Link href={'/products'}><Button className="bg-secondary text-white ">Back</Button></Link></div> :
+        <>
+        <span className='w-full sm:w-1/2 flex ml-auto justify-end pt-4'>
+        <Select
+          className="w-full !border-[1.5px]  !border-blue-gray-200/90 !border-t-blue-gray-200/90 bg-white text-gray-800 ring-4 ring-transparent placeholder:text-gray-600 focus:!border-primary focus:!border-t-blue-gray-900 group-hover:!border-primary"
+          placeholder="Select Category"
+          value={category}
+          onChange={(value) => handleCategoryChange(value)}
+        >
 
-        allProducts.length < 1 ? <div className="flex justify-center items-center max-w-4xl text-4xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">Currently, there are no products available. Stay tuned—exciting new items are coming soon!</div> :
-       allProducts.map((item,index)=>(
-
-            <CardDefault key={index} url={item?.file[0]} navigate={item._id} title={item?.productname} price1={item?.price} price2={`${item?.price }`} quantity={item?.quantity} onAddToCart={() => handleAddToCart(item)} />
-          ))
-        }
-        </div>}
-        <span className={allProducts.length == 0 ? 'flex justify-center items-center py-4':'flex justify-center items-center py-4'}>
-          <DefaultPagination nextPage={nextPage} previousPage={previousPage}/>
-
+          <Option  >Select Category </Option>
+          <Option  value="">All</Option>
+          <Option  value="airbuds">Airbuds</Option>
+          <Option  value="cloths">Cloths</Option>
+          <Option  value="glasses">Glasses</Option>
+          <Option  value="homeAccessories">Home Accessories</Option>
+          <Option  value="perfumes">Perfumes</Option>
+          <Option  value="heads">Heads</Option>
+          <Option  value="watch">Watch</Option>
+          <Option  value="jackets">Jackets</Option>
+        </Select>
         </span>
+        <div className='font grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  justify-items-center gap-4 pb-10 pt-4 flex-grow max-w-screen-xl  w-full min-h-screen max-h-full '>
+      { allProducts.map((item,index)=>(
+        
+            <CardDefault key={index} url={item?.file[0]} navigate={item._id} title={item?.productname} price1={item?.price} price2={`${item?.price }`} quantity={item?.quantity} onAddToCart={() => handleAddToCart(item)} />
+          ))}
+          
+        </div>
+        <span className={allProducts.length == 0 ? ' justify-center items-center py-4 hidden':'flex justify-center items-center py-4'}>
+          <DefaultPagination nextPage={nextPage} previousPage={previousPage}/>
+        </span>
+        </>
+        }
+       
     </Suspense>
   )
 }
