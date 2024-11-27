0dc37f2f0996e126e6955e38b329743f7c893f69
ubaidahmed04 <obaidkhanzada2004@gmail.com>
2024-11-27 13:04:16 +0500

    update not found page

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,17 +1,16 @@
 // src/app/not-found.js
 
 import Link from 'next/link';
-import RootLayout from './layout';
 const NotFoundPage = () => {
   return (
    <div className='flex flex-col justify-center items-center w-full h-screen'>
     <div>
-    <h1 className="text-6xl font-bold text-red-500 text-center">404</h1>
+        <h1 className="text-6xl font-bold text-red-500 text-center">404</h1>
         <h1 className='text-2xl fontbold text-secondary text-center'> Page Not Found</h1>
         <p className="mt-2 text-gray-600">The page you are looking for does not exist.</p>
-        <Link href="/" className="mt-4 px-6 py-2 text-xl myfont bg-gray-700 text-white  rounded-lg hover:bg-blue-600 transition duration-200">
-        Go back to Home
-      </Link>
+        <Link href="/" className="mt-4 px-6 py-2 text-xl myfont bg-secondary text-white  rounded-lg hover:bg-blue-600 transition duration-200">
+        <p className='mt-2 text-gray-600'> Go back to Home </p>
+        </Link>
     </div>
    </div>
   );
